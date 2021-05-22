31ca9590092adbb169898ffb6699cc109e76af06
bbDev <bbdev13@gmail.com>
2021-05-23 00:04:05 +0800

    perbaiki jml tabungan dashboard tiap role

--- app/Helpers.php
+++ app/Helpers.php
@@ -5,6 +5,8 @@ use App\Models\Kelas;
 use App\Models\Pegawai;
 use App\Models\Siswa;
 use App\Models\Tabungan;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 function getJenisKelamin()
 {
@@ -55,5 +57,59 @@ function getJmlTabungan()
 function getJmlPenarikan()
 {
     $sld = Penarikan::sum('jml_tarik');
+    return $sld;
+}
+
+function getJmlSaldo()
+{
+    $sld = getJmlTabungan() - getJmlPenarikan();
+    return $sld;
+}
+
+
+function getJmlTabunganGuru()
+{
+    $pgw_id = Auth::user()->pegawai->user_id;
+    $sld = DB::select('SELECT SUM(tbd.jumlah) jml FROM tabungan_detail tbd
+    WHERE tbd.user_id = ?', [$pgw_id]);
+    return $sld[0]->jml;
+}
+
+function getJmlPenarikanGuru()
+{
+    $pgw_id = Auth::user()->pegawai->user_id;
+    $sld = DB::select('SELECT SUM(pnr.jml_tarik) jml FROM penarikan pnr
+    WHERE pnr.user_id = ?', [$pgw_id]);
+    return $sld[0]->jml;
+}
+
+function getJmlSaldoGuru()
+{
+    $sld = getJmlTabunganGuru() - getJmlPenarikanGuru();
+    return $sld;
+}
+
+
+function getJmlTabunganSiswa()
+{
+    $sw_id = Auth::user()->siswa->id;
+    $sld = DB::select('SELECT SUM(tbd.jumlah) jml FROM tabungan_detail tbd
+    INNER JOIN tabungan tbn ON tbd.tabungan_id=tbn.id
+    WHERE tbn.siswa_id = ?', [$sw_id]);
+    return $sld[0]->jml;
+}
+
+function getJmlPenarikanSiswa()
+{
+    $sw_id = Auth::user()->siswa->id;
+    $sld = DB::select('SELECT SUM(pnr.jml_tarik) jml FROM penarikan pnr
+    INNER JOIN tabungan tbn ON pnr.tabungan_id=tbn.id
+    WHERE tbn.siswa_id = ?', [$sw_id]);
+    return $sld[0]->jml;
+}
+
+function getJmlSaldoSiswa()
+{
+    $sld = getJmlTabunganSiswa() - getJmlPenarikanSiswa();
     return $sld;
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -132,7 +132,7 @@
                             <div class="container-fluid">
                                 <div class="row">
                                     <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
-                                        <span class="txt-dark block counter">{{ getJmlPenarikan() }}</span>
+                                        <span class="txt-dark block counter">{{ number_format(getJmlPenarikan()) }}</span>
                                         <span class="weight-500 uppercase-font block">Penarikan</span>
                                     </div>
                                     <div class="col-xs-6 text-center  pl-0 pr-0 data-wrap-right">
@@ -155,7 +155,7 @@
                             <div class="container-fluid">
                                 <div class="row">
                                     <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
-                                        <span class="txt-dark block counter">{{ number_format(getJmlTabungan()) }}</span>
+                                        <span class="txt-dark block counter">{{ number_format(getJmlSaldo()) }}</span>
                                         <span class="weight-500 uppercase-font block">Saldo</span>
                                     </div>
                                     <div class="col-xs-6 text-center  pl-0 pr-0 data-wrap-right">
@@ -230,7 +230,7 @@
                             <div class="container-fluid">
                                 <div class="row">
                                     <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
-                                        <span class="txt-dark block counter">{{ number_format(getJmlTabungan()) }}</span>
+                                        <span class="txt-dark block counter">{{ number_format(getJmlTabunganGuru()) }}</span>
                                         <span class="weight-500 uppercase-font block">Tabungan</span>
                                     </div>
                                     <div class="col-xs-6 text-center  pl-0 pr-0 pt-25  data-wrap-right">
@@ -255,7 +255,7 @@
                             <div class="container-fluid">
                                 <div class="row">
                                     <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
-                                        <span class="txt-dark block counter">{{ getJmlPenarikan() }}</span>
+                                        <span class="txt-dark block counter">{{ number_format(getJmlPenarikanGuru()) }}</span>
                                         <span class="weight-500 uppercase-font block">Penarikan</span>
                                     </div>
                                     <div class="col-xs-6 text-center  pl-0 pr-0 data-wrap-right">
@@ -278,7 +278,7 @@
                             <div class="container-fluid">
                                 <div class="row">
                                     <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
-                                        <span class="txt-dark block counter">{{ number_format(getJmlTabungan()) }}</span>
+                                        <span class="txt-dark block counter">{{ number_format(getJmlSaldoGuru()) }}</span>
                                         <span class="weight-500 uppercase-font block">Saldo</span>
                                     </div>
                                     <div class="col-xs-6 text-center  pl-0 pr-0 data-wrap-right">
@@ -296,7 +296,7 @@
 @elseif (auth()->user()->level == 'Siswa')
 <div class="row">
     <a href="{{ route('siswa.tabungan') }}">
-        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
             <div class="panel panel-default card-view pa-0">
                 <div class="panel-wrapper collapse in">
                     <div class="panel-body pa-0">
@@ -304,7 +304,7 @@
                             <div class="container-fluid">
                                 <div class="row">
                                     <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
-                                        <span class="txt-dark block counter">{{ number_format(getJmlTabungan()) }}</span>
+                                        <span class="txt-dark block counter">{{ number_format(getJmlTabunganSiswa()) }}</span>
                                         <span class="weight-500 uppercase-font block">Tabungan</span>
                                     </div>
                                     <div class="col-xs-6 text-center  pl-0 pr-0 pt-25  data-wrap-right">
@@ -321,7 +321,7 @@
         </div>
     </a>
     <a href="{{ route('siswa.penarikan') }}">
-        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
             <div class="panel panel-default card-view pa-0">
                 <div class="panel-wrapper collapse in">
                     <div class="panel-body pa-0">
@@ -329,11 +329,34 @@
                             <div class="container-fluid">
                                 <div class="row">
                                     <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
-                                        <span class="txt-dark block counter">{{ getJmlPenarikan() }}</span>
+                                        <span class="txt-dark block counter">{{ number_format(getJmlPenarikanSiswa()) }}</span>
                                         <span class="weight-500 uppercase-font block">Penarikan</span>
                                     </div>
                                     <div class="col-xs-6 text-center  pl-0 pr-0 data-wrap-right">
                                         <i class="fa fa-cut data-right-rep-icon txt-light-grey"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <a href="{{ route('siswa.tabungan') }}">
+        <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+            <div class="panel panel-default card-view pa-0">
+                <div class="panel-wrapper collapse in">
+                    <div class="panel-body pa-0">
+                        <div class="sm-data-box">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-xs-6 text-center pl-0 pr-0 data-wrap-left">
+                                        <span class="txt-dark block counter">{{ number_format(getJmlSaldoSiswa()) }}</span>
+                                        <span class="weight-500 uppercase-font block">Saldo</span>
+                                    </div>
+                                    <div class="col-xs-6 text-center  pl-0 pr-0 data-wrap-right">
+                                        <i class="fa fa-money data-right-rep-icon txt-light-grey"></i>
                                     </div>
                                 </div>
                             </div>
